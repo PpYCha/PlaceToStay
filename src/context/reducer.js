cce07b6c90d2bdc15806334d7f2a426e70fe5978
PpYCha <diasandiaz7@gmail.com>
2022-08-01 11:45:56 +0800

    Login,Register,Alert,Loading

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -2,6 +2,21 @@ import ContextProvider from "./ContextProvider";
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case "OPEN_LOGIN":
+      return { ...state, openLogin: true };
+
+    case "CLOSE_LOGIN":
+      return { ...state, openLogin: false };
+
+    case "START_LOADING":
+      return { ...state, loading: true };
+
+    case "END_LOADING":
+      return { ...state, loading: false };
+
+    case "UPDATE_ALERT":
+      return { ...state, alert: action.payload };
+
     case "UPDATE_USER":
       return { ...state, currentUser: action.payload };
 
