f9df9e94361b6ffdb908d7209a39a9966d076c63
PpYCha <diasandiaz7@gmail.com>
2022-08-09 13:42:20 +0800

    One Tap Login Tutorial

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,16 +1,12 @@
-import ContextProvider from "./ContextProvider";
-
 const reducer = (state, action) => {
   switch (action.type) {
     case "OPEN_LOGIN":
       return { ...state, openLogin: true };
-
     case "CLOSE_LOGIN":
       return { ...state, openLogin: false };
 
     case "START_LOADING":
       return { ...state, loading: true };
-
     case "END_LOADING":
       return { ...state, loading: false };
 
@@ -18,6 +14,7 @@ const reducer = (state, action) => {
       return { ...state, alert: action.payload };
 
     case "UPDATE_USER":
+      localStorage.setItem("currentUser", JSON.stringify(action.payload));
       return { ...state, currentUser: action.payload };
 
     default:
